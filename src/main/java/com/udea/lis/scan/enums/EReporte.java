f02ad0cde675cff3141ddc14ed2141115bc131a1
camilo-avendano1 <camilo.avendano1@udea.edu.co>
2024-03-09 13:50:00 -0500

    Usuario Model

---
+++ src/main/java/com/udea/lis/scan/enums/EReporte.java
@@ -0,0 +1,5 @@
+package com.udea.lis.scan.enums;
+
+public enum EReporte {
+    Hardware, Software, Otro
+}
---
+++ src/main/java/com/udea/lis/scan/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.udea.lis.scan.repository;
+
+import com.udea.lis.scan.model.entity.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Integer>{
+}
