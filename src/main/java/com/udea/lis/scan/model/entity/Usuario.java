18049873044ab8c3ec41637150eeb5cba2064c5f
camilo-avendano1 <camilo.avendano1@udea.edu.co>
2024-03-09 12:09:49 -0500

    Create Usuario.java
    create usuario java

---
+++ src/main/java/com/udea/lis/scan/model/entity/Usuario.java
@@ -0,0 +1,16 @@
+package com.udea.lis.scan.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    
+}
