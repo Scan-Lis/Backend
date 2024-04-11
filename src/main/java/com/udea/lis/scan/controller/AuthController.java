ac2a5d4e2579fe56e666fb1fdb197d9650347c22
Jair Leal <jair.leal@udea.edu.co>
2024-04-11 18:00:02 -0500

    manejo de excepciones login y registro

--- src/main/java/com/udea/lis/scan/controller/AuthController.java
+++ src/main/java/com/udea/lis/scan/controller/AuthController.java
@@ -4,6 +4,10 @@ import com.udea.lis.scan.model.dto.UsuarioDTO;
 import com.udea.lis.scan.model.dto.auth.LoginUserDto;
 import com.udea.lis.scan.model.dto.auth.ResponseAuth;
 import com.udea.lis.scan.service.Auth.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,17 +19,31 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
-
     private AuthService AuthService;
+    @Operation(summary = "Login", description = "Login para obtener token de autenticación", tags = { "auth" } , responses = {
+            @ApiResponse(responseCode = "200", description = "Successful login", content = @Content(schema = @Schema(implementation = ResponseAuth.class ))),
+            @ApiResponse(responseCode = "400", description = "Invalid user/password", content = @Content(schema = @Schema(implementation = String.class))) })
 
     @PostMapping("/login")
-    public ResponseEntity<ResponseAuth> login(@RequestBody LoginUserDto loginUserDto){
-        return new ResponseEntity<>(AuthService.login(loginUserDto), HttpStatus.OK);
+    public ResponseEntity<?> login(@RequestBody LoginUserDto loginUserDto){
+        try {
+            return new ResponseEntity<>(AuthService.login(loginUserDto), HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Usuario o contraseña incorrectos");
+        }
     }
 
+    @Operation(summary = "Registro", description = "Registro de usuario", tags = { "auth" }, responses = {
+            @ApiResponse(responseCode = "201", description = "Usuario creado", content = @Content(schema = @Schema(implementation = ResponseAuth.class)) ),
+            @ApiResponse(responseCode = "400", description = "Error al registrar usuario", content = @Content(schema = @Schema(implementation = String.class))) })
     @PostMapping("/register")
-    public ResponseEntity<ResponseAuth> register(@RequestBody UsuarioDTO createUserDto){
-        return new ResponseEntity<>(AuthService.registro(createUserDto), HttpStatus.CREATED);
+    public ResponseEntity<?> register(@RequestBody UsuarioDTO createUserDto){
+        try {
+            return new ResponseEntity<>(AuthService.registro(createUserDto), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Usuario o contraseña incorrectos");
+        }
+
     }
 
 
