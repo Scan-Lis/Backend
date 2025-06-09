c9dba7380edcc38f56f76883954e21b462a10467
Jair Leal <jair.leal@udea.edu.co>
2025-06-08 22:15:17 -0500

    proteccion de endpoints

--- src/main/java/com/udea/lis/scan/config/CorsConfig.java
+++ src/main/java/com/udea/lis/scan/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // permite todas las rutas
                 .allowedOrigins("*") // permite todos los orígenes
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
+                //.allowCredentials(true);
     }
 }
